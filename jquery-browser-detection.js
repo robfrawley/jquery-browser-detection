36320c643572e25d8efff9ba0f835e9ad21600dc
Rob Frawley 2nd (Scribe Inc) <rfrawley@scribenet.com>
2014-03-15 22:38:29 -0400

    cleanup

--- jquery-browser-detection.js
+++ jquery-browser-detection.js
@@ -1,11 +1,7 @@
-// Limit scope pollution from any deprecated API
 (function() {
 
     var matched, browser;
 
-    // Use of jQuery.browser is frowned upon.
-    // More details: http://api.jquery.com/jQuery.browser
-    // jQuery.uaMatch maintained for back-compat
     jQuery.uaMatch = function( ua ) {
         ua = ua.toLowerCase();
 
@@ -17,46 +13,49 @@
             [];
 
         return {
-            browser: match[ 1 ] || "",
-            version: match[ 2 ] || "0"
+            browser: match[1] || "",
+            version: match[2] || "0"
         };
     };
 
     matched = jQuery.uaMatch( navigator.userAgent );
     browser = {};
 
-    if ( matched.browser ) {
-        browser[ matched.browser ] = true;
+    if (matched.browser) {
+        browser[matched.browser] = true;
         browser.version = matched.version;
     }
 
-    // Chrome is Webkit, but Webkit is also Safari.
-    if ( browser.chrome ) {
+    if (browser.chrome) {
         browser.webkit = true;
-    } else if ( browser.webkit ) {
+    } else if (browser.webkit) {
         browser.safari = true;
     }
 
     jQuery.browser = browser;
 
     jQuery.sub = function() {
-        function jQuerySub( selector, context ) {
-            return new jQuerySub.fn.init( selector, context );
+        function jQuerySub(selector, context) {
+            return new jQuerySub.fn.init(selector, context);
         }
-        jQuery.extend( true, jQuerySub, this );
-        jQuerySub.superclass = this;
-        jQuerySub.fn = jQuerySub.prototype = this();
+        jQuery.extend(true, jQuerySub, this);
+
+        jQuerySub.superclass     = this;
+        jQuerySub.fn             = jQuerySub.prototype = this();
         jQuerySub.fn.constructor = jQuerySub;
-        jQuerySub.sub = this.sub;
-        jQuerySub.fn.init = function init( selector, context ) {
-            if ( context && context instanceof jQuery && !(context instanceof jQuerySub) ) {
+        jQuerySub.sub            = this.sub;
+
+        jQuerySub.fn.init = function init(selector, context) {
+            if (context && context instanceof jQuery && !(context instanceof jQuerySub)) {
                 context = jQuerySub( context );
             }
 
-            return jQuery.fn.init.call( this, selector, context, rootjQuerySub );
+            return jQuery.fn.init.call(this, selector, context, rootjQuerySub);
         };
+
         jQuerySub.fn.init.prototype = jQuerySub.fn;
         var rootjQuerySub = jQuerySub(document);
+
         return jQuerySub;
     };
 
